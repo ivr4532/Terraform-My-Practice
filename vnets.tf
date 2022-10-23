db041a05b1792cedb0ee998826a0808c23b8e90e
IVR4532 <vanga.cloud@gmail.com>
2022-10-22 19:53:18 -0500

    count added for Subnets

--- vnets.tf
+++ vnets.tf
@@ -1,3 +1,10 @@
+# subnet address range specification
+variable "subnet_range" {
+  default = ["10.20.0.0/24", "10.20.1.0/24", "10.20.2.0/24"]
+  type = list(string)
+  
+}
+
 # Create Virtual Network
 resource "azurerm_virtual_network" "myvent" {
   resource_group_name = azurerm_resource_group.devops_rg.name
@@ -9,9 +16,10 @@ resource "azurerm_virtual_network" "myvent" {
 
 # Create Network Subnet
 resource "azurerm_subnet" "mysubnet" {
-    name = "mysubnet-1"
+  count = 3
+    name = "az-subnet-${count.index+1}"
     resource_group_name = azurerm_resource_group.devops_rg.name
     virtual_network_name = azurerm_virtual_network.myvent.name
-    address_prefixes = ["10.20.0.0/24"]
+    address_prefixes = [element(var.subnet_range, count.index)]
   
 }
